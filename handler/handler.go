fe2369b7f2ef0ed7adddd10aab38230203cc6b00
Wei Sun <swyddz1989@gmail.com>
2018-10-07 09:51:43 -0700

    cors

--- handler/handler.go
+++ handler/handler.go
@@ -137,6 +137,6 @@ func (h *Handler) CreateJsonRpc(w http.ResponseWriter, r *http.Request) {
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, OPTIONS, HEAD, CONNECT, TRACE, PATCH")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Access-Control-Request-Headers, Access-Control-Request-Method, Connection, Host, Origin, User-Agent, Referer, Cache-Control, X-header")
 }
